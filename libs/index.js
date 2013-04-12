c6e99327a65475f0e90e367400214e7dcb42e794
Ismael Gorissen <igorissen@PP-Ismael.local>
2013-04-12 16:49:33 +0200

    update class name

--- libs/index.js
+++ libs/index.js
@@ -3,7 +3,10 @@
  * Date: 10/04/13 12:50
  * Company: PinchProject
  * Website: http://pinchproject.com/
+ *
+ * Copyright (C) 2013 PinchProject
+ * MIT Licensed
  */
 
-exports.Message = require('./message.js');
-exports.Sender = require('./sender.js');
+exports.Message = require('./Message.js');
+exports.Sender = require('./Sender.js');
